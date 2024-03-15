8fbfee9322f7bfa1d28630a74f27cc7feefea73f
Noe <8528445+NoeRGuerra@users.noreply.github.com>
2024-03-15 06:57:49 -0400

    Rewrite button click function
    Rewrite the button click function to not display the operator to comply with project's instructions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const DIGITS = 12;
+const MAXDIGITS = 12;
 
 function add(number1, number2) {
     return number1 + number2;
@@ -36,7 +36,7 @@ function operate(operand1, operator, operand2) {
             break;
     }
     if (result.toString().length > 12){
-        decimalPlaces = DIGITS - parseInt(result).toString().length;
+        decimalPlaces = MAXDIGITS - parseInt(result).toString().length;
         result = result.toFixed(decimalPlaces);
     }
     return result;
@@ -51,58 +51,61 @@ function isNumber(value){
 }
 
 const buttons = document.querySelectorAll(".button-grid button");
-const regexp = /[0-9+\-*/]/
 const display = document.querySelector("#display");
 let operationArray = [];
-let result = 0;
+let clickedButton;
+let previousButton;
+let displayValue;
+let result;
 buttons.forEach((button) => {
     button.addEventListener('click', (event) => {
-        let pushedButton = event.target.value;
-        let displayValue = display.textContent;
-
-        if (isNumber(pushedButton)){
-            if (isNumber(displayValue) && displayValue.length < 12){
-                display.textContent = (displayValue == "0" || operationArray[0] == "=") ? pushedButton : displayValue + pushedButton;
-            }
-            else if (isOperator(displayValue)){
-                if (operationArray.length == 3){
-                    result = operate(...operationArray);
-                    operationArray = [];
-                    operationArray.push(result);
+        displayValue = display.textContent;
+        clickedButton = event.target;
+        if (isNumber(clickedButton.value)){
+            if (displayValue.length < 12 ){ // Check if clicked number must be appended or if it must replace the display value
+                if (previousButton){
+                    display.textContent = (displayValue == "0" || isOperator(previousButton.value)) ? clickedButton.value : displayValue + clickedButton.value;
                 }
-                operationArray.push(displayValue);
-                display.textContent = pushedButton;
+                else{
+                    display.textContent = (displayValue == "0") ? clickedButton.value : displayValue + clickedButton.value;
+                }
+            }
+            if (previousButton && isOperator(previousButton.value)){ // Add displayed number to the array when clicking on number(s) afte clicking on operator
+                display.textContent = clickedButton.value;
+                operationArray.push(previousButton.value);
             }
         }
-        else if (isOperator(pushedButton)){
-            if (isNumber(displayValue) && displayValue != "0"){
+        else if (isOperator(clickedButton.value)){
+            if (previousButton && ( isNumber(previousButton.value) || previousButton.value === "=") ){
                 operationArray.push(displayValue);
             }
-            else if (displayValue == "0"){
+            if (operationArray.length === 3){
+                result = operate(...operationArray);
+                operationArray = [result,];
+                display.textContent = result;
+            }
+        }
+        else if (clickedButton.value == "="){
+            if (!previousButton){
                 return;
             }
-            display.textContent = pushedButton;
-        }
-        else if (pushedButton == "="){
-            if (operationArray.length < 1)
-                return;
-            if (isNumber(displayValue))
+            if (isNumber(previousButton.value)){
                 operationArray.push(displayValue);
-            result = operate(...operationArray);
-            display.textContent = result;
-            operationArray = [];
-            operationArray.push("=");
-            console.log(operationArray);
+            }
+            else if (isOperator(previousButton.value)){
+                operationArray.push(previousButton.value);
+            }
+            if (operationArray.length === 3){
+                result = operate(...operationArray);
+            }
+            else if (operationArray.length === 2){
+                result = operate(operationArray[0], operationArray[1], operationArray[0]);
+            }
+            if (result){
+                display.textContent = result;
+                operationArray = [];
+            }
         }
-        else if (pushedButton == "clear"){
-            operationArray = [];
-            display.textContent = "0";
-        }
-        else if (pushedButton == "sign"){
-            if (isNumber(displayValue) && displayValue != "0")
-                display.textContent = parseFloat(displayValue)*-1;
-        }
-        console.log(operationArray);
-        console.log(`Last pushed: ${pushedButton}`);
+        previousButton = clickedButton;
     })
 })
